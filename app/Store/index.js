2d3b20b4d51aa47c55364674d280fde5b57fc262
Philippine CLUNIAT <pcluniat@pentalog.com>
2021-01-22 14:38:25 +0100

    add redux persist

--- app/Store/index.js
+++ app/Store/index.js
@@ -3,13 +3,27 @@ import {
   combineReducers,
   getDefaultMiddleware,
 } from '@reduxjs/toolkit';
+import {persistStore, persistReducer} from 'redux-persist';
+import AsyncStorage from '@react-native-community/async-storage';
 import {authReducer} from './Auth/reducers';
 
 const rootReducer = combineReducers({
   auth: authReducer,
 });
 
-export const store = configureStore({
-  reducer: rootReducer,
+const persistConfig = {
+  key: 'root',
+  whitelist: ['auth'],
+  storage: AsyncStorage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
   middleware: getDefaultMiddleware({serializableCheck: false}),
 });
+
+const persistor = persistStore(store);
+
+export {store, persistor};
